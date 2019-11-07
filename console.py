61be3998fd35ba518331313cbe2f8f957b05be76
Santiago Moreno <pysancode@gmail.com>
2019-11-06 21:07:56 -0300

    LIBRO MAYOR FUNCIONANDO 100% REAL NO FAKE WOOOOOo

--- console.py
+++ console.py
@@ -1,61 +1,3 @@
-#activo = {
-#"1.01": """BANCO "X" Cuenta Corriente""",
-#"1.02": "Caja",
-#"1.03": "Deudores por ventas",
-#"1.04": "Documentos a cobrar",
-#"1.05": "Equipos de computacion",
-#"1.06": "Inmuebles",
-#"1.07": "Instalaciones",
-#"1.08": "Maquinarias",
-#"1.09": "Materias primas",
-#"1.10": "Mercaderias",
-#"1.11": "Muebles y utiles",
-#"1.12": "Rodados",
-#"1.13": """Tarjetas de credito "XX" """,
-#"1.14": "Valores a depositar
-#}
-
-#pasivo = {
-#    "2.01": "Acreedores varios",
-#    "2.02": "Documentos a pagar",
-#    "2.03": "Proovedores",
-#    "2.04": "Valores diferidos a pagar"
-#}
-#
-#patrimonio_neto = {
-#    "3.01": "Capital"
-#}
-#
-#resultado_negativo = {
-#    "4.01": "Alquileres perdidos",
-#    "4.02": "Comisiones perdidas",
-#    "4.03": "Costo de mercaderias vendidas",
-#    "4.04": "Descuentos cedidos",
-#    "4.05": "Fletes y acarreos",
-#    "4.06": "Gastos generales",
-#    "4.07": "Impuestos",
-#    "4.08": "Intereses perdidos",
-#    "4.09": "Publicidad perdida",
-#    "4.10": "Seguros",
-#    "4.11": "Sueldos y jornales"
-#}
-#
-#resultado_positivo = {
-#    "5.01": "Alquileres ganados",
-#    "5.02": "Comisiones ganadas",
-#    "5.03": "Descuentos obtenidos",
-#    "5.04": "Intereses ganados",
-#    "5.05": "Ventas"
-#}
-#
-#valid = [
-#    "1.01","1.02","1.03","1.04","1.05","1.06","1.07","1.08","1.09","1.10","1.11","1.12","1.13","1.14",
-#    "2.01","2.02","2.03","2.04",
-#    "3.01",
-#    "4.01","4.02","4.03","4.04","4.05","4.06","4.07","4.08","4.09","4.10","4.11",
-#    "5.01","5.02","5.03","5.04","5.05"
-#
-
 __version__ = "1.0"
 
 segun_dict = {
@@ -706,23 +648,7 @@ def display_libro_mayor(libro):
 
 
 
-"""
-| Caja(1.02)                              |
-|-----------------------------------------|
-| Debe               | Haber              |
-|-----------------------------------------|
-| 1: 45000           | 5: 6500            |
-|                    | 6: 10000           |
-|                    |                    |
-|                    |                    |
-|                    |                    |
-|                    |                    |
-|-----------------------------------------|
-| 45000              | 16500              |
-|-----------------------------------------|
-| Saldo Deudor: 28500                     |
-|-----------------------------------------|
-"""
+
 
 def main():
     print("AlfaBOOK Edicion Consola\n",
@@ -870,6 +796,24 @@ display_libro_mayor(libro)
  |2000    | 5000    |
  |------------------|
  |Saldo A: 5000     |
+"""
+
+"""
+| Caja(1.02)                              |
+|-----------------------------------------|
+| Debe               | Haber              |
+|-----------------------------------------|
+| 1: 45000           | 5: 6500            |
+|                    | 6: 10000           |
+|                    |                    |
+|                    |                    |
+|                    |                    |
+|                    |                    |
+|-----------------------------------------|
+| 45000              | 16500              |
+|-----------------------------------------|
+| Saldo Deudor: 28500                     |
+|-----------------------------------------|
 """
 
 """
